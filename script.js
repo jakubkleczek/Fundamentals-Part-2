3ed1cddf722165bdb4505d026f3c910bff83853d
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-14 16:13:58 +0100

    Functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,25 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const interface = 'Audio';
 // const private = 534;
 // const if = 23;
+*/
+
+function logger() {
+    console.log('My name is Jacob');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
