0c56ac153383a40eb7dcebe84783f7d9798240b7
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-15 15:50:03 +0100

    Reviewing Functions

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ console.log(yearsUntilRetirement(2002, 'Kuba'));
 console.log(yearsUntilRetirement(1996, 'Santiago'));
 */
 
+/*
 function cutFruitPieces(fruit) {
     return fruit * 4;
 }
@@ -83,3 +84,26 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🥳`);
+        return -1;
+    }
+
+}
+console.log(yearsUntilRetirement(2002, 'Kuba'))
+console.log(yearsUntilRetirement(1970, 'Mike'))
+
+
