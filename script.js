d16fe21f0725fc9c9c9cb8e830c2e0221824d04e
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-15 12:54:29 +0100

    Arrow Functions

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 */
 
+/*
 // Function declaration
 function calcAge1(birthYear) {
     return 2037 - birthYear;
@@ -48,3 +49,19 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(2002);
 
 console.log(age1, age2);
+*/
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(2002);
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(2002, 'Kuba'));
+console.log(yearsUntilRetirement(1996, 'Santiago'));
