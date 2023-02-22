ba06e79ed219eacdb6018167a80eec7257d85767
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-22 17:03:09 +0100

    Introduction to Arrays

--- script.js
+++ script.js
@@ -87,6 +87,9 @@ console.log(fruitProcessor(2, 3));
 */
 
 /*
+
+///////////////////////////////////
+// Reviewing Functions
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
@@ -108,3 +111,35 @@ console.log(yearsUntilRetirement(2002, 'Kuba'))
 console.log(yearsUntilRetirement(1970, 'Mike'))
 */
 
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1])
+
+friends[2] = 'Jay';
+console.log(friends);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]),
+calcAge(years[years.length - 1])];
+console.log(ages)
