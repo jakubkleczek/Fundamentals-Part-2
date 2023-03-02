54e71ccd6c1b1d25cc6dca3c6e402698d4077867
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-02 11:22:41 +0100

    Object Methods

--- script.js
+++ script.js
@@ -194,7 +194,7 @@ const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
 console.log(bills, tips, total)
 */
 
-
+/*
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
@@ -228,3 +228,39 @@ console.log(jonas['last' + nameKey]);
 // Challenge
 
 console.log(jonas.firstName + ' has ' + jonas.friends.length + ' friends, and his best friend is called ' + jonas.friends[0]);
+*/
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function(birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function() {
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is ${this.age}-years old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// Challenge 
+console.log(jonas.getSummary());
