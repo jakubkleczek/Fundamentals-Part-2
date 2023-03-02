05c2d507a6d3c462b4f597cc866b629a48f8ed9f
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-02 10:42:43 +0100

    Dot vs. Bracket Notation

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ if (friends.includes('Steven')) {
 */
 
 
-
+/*
 function calcTip(bill) {
     return bill >=50 && bill <=300 ? bill * 0.15 : bill * 0.2;
 }
@@ -192,3 +192,39 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
 console.log(bills, tips, total)
+*/
+
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+ 
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+// console.log(jonas[interestedIn]);
+
+// if(jonas[interestedIn]) {
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job, and friends')
+// }
+
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschemdtman';
+
+
+// Challenge
+
+console.log(jonas.firstName + ' has ' + jonas.friends.length + ' friends, and his best friend is called ' + jonas.friends[0]);
