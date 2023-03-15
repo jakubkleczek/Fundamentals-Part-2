2f65e7939344d6d1b7b8cd3a45a82af9f5fecad3
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-15 10:35:47 +0100

    The while Loop

--- script.js
+++ script.js
@@ -359,7 +359,7 @@ for(let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof jonas[i]);
 }
 */
-
+/*
 const jonas = [
     'Jonas',
     'Schmedtmann',
@@ -381,4 +381,24 @@ for(let exercise = 1; exercise < 4; exercise++) {
     for(let rep = 1; rep < 6; rep++) {
         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋🏽`)
     }
+}
+*/
+
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋🏽`);
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep} 🏋🏽`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;  
+// console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
 }
