586f8abb5dce35682016332fad9bc0c03d1ca233
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-15 09:51:07 +0100

    Looping Arrays, Breaking and Continuing

--- script.js
+++ script.js
@@ -315,16 +315,46 @@ for(let rep = 1; rep <= 10; rep++) {
 }
 */
 
-/*
-const jonasArray = [
+const jonas = [
     'Jonas',
     'Schmedtmann',
     2037 - 1991,
     'teacher',
-    ['Michael', 'Peter', 'Steven']
+    ['Michael', 'Peter', 'Steven'],
+    true
 ];
+const types = [];
 
-for(let i = 0; i < jonasArray.length; i++) {
-    console.log(jonasArray[i], typeof jonasArray[i])
+for(let i = 0; i < jonas.length; i++) {
+    // Reading from jonas array
+    console.log(jonas[i], typeof jonas[i]);
+
+    // Filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
 }
-*/
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break 
+console.log('--- ONLY STRINGS ---');
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---');
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
